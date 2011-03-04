f5cb30231d1a446d872579578c23fe2e07925e7c
anthonator <anthony@sticksnleaves.com>
2011-03-03 19:55:19 -0500

    DRYed up error handling logic.

--- lib/skittles/request.rb
+++ lib/skittles/request.rb
@@ -5,31 +5,31 @@ require 'hashie'
 module Skittles
 	module Request
 		# Perform an HTTP GET request
-		def get(path, options = {}, raw = false)
-			request(:get, path, options, raw)
+		def get(path, options = {}, headers = {}, raw = false)
+			request(:get, path, options, headers, raw)
 		end
 		
 		# Performs an HTTP POST request
-		def post(path, options = {}, raw = false)
-			request(:post, path, options, raw)
+		def post(path, options = {}, headers = {}, raw = false)
+			request(:post, path, options, headers, raw)
 		end
 		
 		# Performs an HTTP PUT request
-		def put(path, options = {}, raw = false)
-		  request(:put, path, options, raw)
+		def put(path, options = {}, headers = {}, raw = false)
+		  request(:put, path, options, headers, raw)
 		end
 		
 		# Performs an HTTP DELETE request
-		def delete(path, options = {}, raw = false)
-		  request(:delete, path, options, raw)
+		def delete(path, options = {}, headers = {}, raw = false)
+		  request(:delete, path, options, headers, raw)
 		end
 		
 		private
 		  #Perform an HTTP request
-		  def request(method, path, options, raw)
-		  	headers = {
+		  def request(method, path, options, headers, raw)
+		  	headers.merge!({
 		  		'User-Agent' => user_agent
-		  	}
+		  	})
 		  	
 		  	options.merge!({
 		  		:client_id     => client_id,
@@ -38,43 +38,14 @@ module Skittles
 		  	
 		  	begin
 		  	  response = connection.request(method, paramify(path, options), headers)
-		  	rescue OAuth2::AccessDenied => e
-		  	  info = parse_json(e.response.body).meta
-		  	  error = Skittles::Unauthorized.new
-		  	  error.code = info.code.to_i
-		  	  error.type = info.errorType
-		  	  error.detail = info.errorDetail
-		  	  raise error
-		  	rescue OAuth2::HTTPError => e
-		  	  info = parse_json(e.response.body).meta
-		  	   case info.code.to_i
-		  	   when 400
-		  	     error = Skittles::BadRequest.new
-		  	   when 401
-		  	     error = Skittles::Unauthorized.new
-		  	   when 404
-		  	     error = Skittles::NotFound.new
-		  	   when 405
-		  	     error = Skittles::MethodNotAllowed.new
-		  	   when 500
-		  	     error = Skittles::InternalServerError.new
-		  	   when 502
-		  	     error = Skittles::BadGateway.new
-		  	   when 503
-		  	     error = Skittles::ServiceUnavailable.new
-		  	   when 504
-		  	     error = Skittles::GatewayTimeout.new
-		  	   else
-		  	     error = Skittles::Error.new
-		  	   end
-		  	   error.code = info.code.to_i
-		  	   error.type = info.errorType
-		  	   error.detail = info.errorDetail
-		  	   raise error
+		  	rescue OAuth2::AccessDenied || OAuth2::HTTPError => e
+		  	  Skittles::Utils.handle_foursquare_error(e.response)
+		  	else
+		  	  Skittles::Error
 		  	end
 		  	
 		  	unless raw
-		  		result = parse_json(response)
+		  		result = Skittles::Utils.parse_json(response)
 		  	end
 		  	
 		  	raw ? response : result.response
@@ -83,11 +54,6 @@ module Skittles
 		  # Encode path and turn params into HTTP query.
       def paramify(path, params)
         URI.encode("#{path}?#{params.map { |k,v| "#{k}=#{v}" }.join('&')}")
-      end
-      
-      # Parses JSON and returns a Hashie::Mash
-      def parse_json(json)
-        Hashie::Mash.new(Yajl::Parser.new.parse(json))
       end
 	end
 end
