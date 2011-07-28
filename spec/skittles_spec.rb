5ed331fe468e1c33ed1ac988b92afe2d6edf052a
= <=>
2011-07-28 19:11:29 -0400

    Fixed venue_search to rely on venues array rather than the old groups array. Special thanks to felipecsl!

--- spec/skittles_spec.rb
+++ spec/skittles_spec.rb
@@ -1,7 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Skittles" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it 'should search for venues' do
+    response = Skittles.venue_search('40.7,-75', :query => 'Brooklyn Bridge Park - Pier 1')
+    response.should_not be_nil
+    response.should be_a_kind_of Array
   end
 end
