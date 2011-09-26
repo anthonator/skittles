446bd11baff3a56236985df60cd6fb76910e1eee
Luca Marra <ldm.marra@gmail.com>
2011-09-26 15:54:44 +0300

    removed empty "puts"

--- lib/skittles/connection.rb
+++ lib/skittles/connection.rb
@@ -8,7 +8,6 @@ module Skittles
 		  	options = {
 		  		:site => endpoint
 		  	}
-		  	puts
 		  	client = OAuth2::Client.new(client_id, client_secret, options)
 		  	oauth_token = OAuth2::AccessToken.new(client, access_token)
 		  	
