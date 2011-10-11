ef2121e221d9ab2ff952df6255846a5debb14f45
Anthony Smith <=>
2011-10-10 21:46:57 -0400

    Updated connection code to work with oauth2 0.5.1 gem.

--- lib/skittles/connection.rb
+++ lib/skittles/connection.rb
@@ -5,14 +5,11 @@ module Skittles
 	module Connection
 		private
 		  def connection
-		  	options = {
-		  		:site => endpoint
-		  	}
-		  	puts
-		  	client = OAuth2::Client.new(client_id, client_secret, options)
-		  	oauth_token = OAuth2::AccessToken.new(client, access_token)
-		  	
-		  	oauth_token.token_param = 'oauth_token'
+		  	client = OAuth2::Client.new(client_id, client_secret, { :site => endpoint })
+		  	oauth_token = OAuth2::AccessToken.new(client, access_token, {
+		  	  :mode => :query, 
+		  	  :param_name => 'oauth_token'
+		  	})
 		  	oauth_token
 		  end
 	end
